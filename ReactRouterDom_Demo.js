9ff1d0074339403c3e19176e0553171aa85b7d1b
Goutamraj Sahu <64259409+GoutamrajSahu@users.noreply.github.com>
2022-09-07 15:07:50 +0530

    Create ReactRouterDom_Demo.js
    Initial commit

---
+++ ReactRouterDom_Demo.js
@@ -0,0 +1,30 @@
+import { Routes, Route, Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="invoices" element={<Invoices />}>
+        <Route path=":invoiceId" element={<Invoice />} />
+        <Route path="sent" element={<SentInvoices />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Invoices() {
+  return (
+    <div>
+      <h1>Invoices</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+function Invoice() {
+  let { invoiceId } = useParams();
+  return <h1>Invoice {invoiceId}</h1>;
+}
+
+function SentInvoices() {
+  return <h1>Sent Invoices</h1>;
+}
